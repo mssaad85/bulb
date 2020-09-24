4659803306f1298e3249a1390659174619f32c26
Muhammad Saad <mssaad85@gmail.com>
2020-09-24 22:57:00 +0500

    Bulb App

---
+++ app.js
@@ -0,0 +1,9 @@
+function workingBulbOn(){
+    var a = document.getElementById("bulbFun")
+    a.src = "imgbulb.jpg"
+}
+function workingBulbOff(){
+    var b = document.getElementById("bulbFun")
+    b.src = "offbulb.jpg"
+
+}
